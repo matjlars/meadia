8b35588f419315e6e5a8d931e62a46f817575241
Matt Larson <matjlars@gmail.com>
2021-11-11 17:57:46 -0600

    added funky

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<h2>Homepage</h2>
+
+<p>To get started, visit <a href="/admin">the admin portal</a> to set up the database</p>
+<p>Then, replace this page (public/index.php) with your homepage!</p>
+<p>Don't forget to check out the README if you have any questions!</p>
